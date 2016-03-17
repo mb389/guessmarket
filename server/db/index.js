0707c3d0d49ca62db40cdc788cca46d8626e94bd
Michael Bushoy <mbushoy@gmail.com>
2016-03-17 15:11:18 -0400

    final

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 var path = require('path');
 var chalk = require('chalk');
 
-var DATABASE_URI = process.env.MONGOLAB_URI;
+var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
 var mongoose = require('mongoose');
 var db = mongoose.connect(DATABASE_URI).connection;
